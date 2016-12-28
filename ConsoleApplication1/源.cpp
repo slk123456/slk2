e83a117935d37d57e90ddc96a05ae17e84f1080a
SLK123456 <1158044678@qq.com>
2016-12-28 15:39:52 +0800

    lalalal

---
+++ ConsoleApplication1/源.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int n, num, temp;
+	while ((cin >> n) && n != 0) {
+		int sum = 0;
+		cin >> num;
+		sum += num * 6 + 5;
+		n--;
+		while (n--) {
+			temp = num;
+			cin >> num;
+			if (temp>num)
+				sum += (temp - num) * 4 + 5;
+			else if (temp<num)
+				sum += (num - temp) * 6 + 5;
+			else
+				sum += 5;
+		}
+		cout << sum << endl;
+	}
+	return 0;
+}
